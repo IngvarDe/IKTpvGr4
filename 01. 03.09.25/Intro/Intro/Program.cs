2aa91bb2185ccda1a46a36b6d70ee070bfc8c9e0
MSI\ingva <inde999@gmail.com>
2025-09-03 10:22:13 +0300

    CHastagIntro

---
+++ 01. 03.09.25/Intro/Intro/Program.cs
@@ -0,0 +1,22 @@
+﻿//namespace on selleks, et näha projekti nime
+//tegemist on projekti aadressiga
+namespace Intro
+{
+    //classi nimi
+    //internal tähendab classi ligipääsutaset
+    internal class Program
+    {
+        //see on meetod Main
+        static void Main(string[] args)
+        {
+            //see on kommentaar ja seda ei loeta koodi käivitamisel
+            //kommentaari saab igasuguseid asju kirjutada
+            Console.WriteLine("1Hello!");
+            Console.WriteLine("2Hello!");
+            Console.WriteLine("3Hello!");
+            Console.WriteLine("4Hello!");
+            Console.WriteLine("5Hello!");
+            Console.WriteLine("6Hello!");
+        }
+    }
+}
