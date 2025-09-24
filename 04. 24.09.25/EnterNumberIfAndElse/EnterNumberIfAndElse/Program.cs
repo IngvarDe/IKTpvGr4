92c193523ee660860f604fd60e99d6fef9c51a2d
MSI\ingva <inde999@gmail.com>
2025-09-24 11:41:36 +0300

    IfAndElseFlowChart2

---
+++ 04. 24.09.25/EnterNumberIfAndElse/EnterNumberIfAndElse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EnterNumberIfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta numbrid 1, 2 või 3");
+
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+            //teha vooskeem e flowchart sellest rakendusest
+            if (number == 1) 
+            {
+                Console.WriteLine("Sisestasid number 1");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Sisestasid number 2");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("Sisestasid number 3");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
