5892a14e27f7a5c05b758983f4f8edf83046eb09
MSI\ingva <inde999@gmail.com>
2025-09-24 10:56:50 +0300

    IfAndElseFlowChart

---
+++ 04. 24.09.25/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta nimi");
+
+            string muutuja = Console.ReadLine();
+
+            if (muutuja == "")
+            {
+                Console.WriteLine("Ei sisestanud nime");
+            }
+            else 
+            {
+                Console.WriteLine("Sisestasid nime " + muutuja);
+            }
+        }
+    }
+}
