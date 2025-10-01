3fd26e31706b1db0f71e1172ba3d5df5fba4eb47
MSI\ingva <inde999@gmail.com>
2025-10-01 09:31:10 +0300

    IfAndElseOddNumbersFlowchart

---
+++ 05. 01.10.25/OddNumberIfAndElse/OddNumberIfAndElse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OddNumberIfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //konsool küsib nr ja muudate selle int andmetüübiks
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+
+            //teha if ja else-ga kontroll, kus saame teada,
+            //et kas nr on paaris või paaritu
+            if (number %2 == 0)
+            {
+                Console.WriteLine("Sisestasid paaris arvu: " + number);
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid paaritu arvu: " + number);
+            }
+            //teha vooskeem e flowchart
+        }
+    }
+}
