49d5693211098edcb82ebe34b6760eff05809d15
MSI\ingva <inde999@gmail.com>
2025-10-01 09:51:17 +0300

    MethodCall

---
+++ 05. 01.10.25/IfAndElseMethodCall/IfAndElseMethodCall/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IfAndElseMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //kutsuda DoSomething meetod välja
+            //method call e meetodi välja kutsumine
+            DoSomething();
+        }
+
+
+        static void DoSomething()
+        {
+            Console.WriteLine("See on DoSomething meetod");
+        }
+    }
+}
