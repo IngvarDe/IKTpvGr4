8390600db54495282882bc90671c651552a55beb
MSI\ingva <inde999@gmail.com>
2025-10-01 11:01:55 +0300

    IfAndElseMethodCallFlowchart2

---
+++ 05. 01.10.25/IfAndElseMethodCall2/IfAndElseMethodCall2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace IfAndElseMethodCall2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number!");
+
+            string nr = Console.ReadLine();
+            int number = Convert.ToInt32(nr);
+
+            if (number % 2 == 0)
+            {
+                //siia tuleb kutsuda esile meetod, kus on kirjas: sisestasid paaris arvu
+                EvenNumber();
+            }
+            else 
+            {
+                // siia tuleb kutsuda esile meetod, kus on kirjas: sisestasid paaritu arvu
+                OddNumber();
+            }
+        }
+
+        static void EvenNumber()
+        {
+            Console.WriteLine("Tegemist on paaris numbriga");
+        }
+
+        static void OddNumber()
+        {
+            Console.WriteLine("Tegemist on paaritu numbriga");
+        }
+        //tuleb teha meetod EvenNumber()
+        //Kui kutsutakse välja see meetod, siis tuleb vastus:
+        //Tegemist on paaris numbriga
+
+        //tuleb teha teine meetod OddNumber()
+        //Kui kutsutakse välja see meetod, siis tuleb vastus:
+        //Tegemist on paaritu numbriga
+    }
+}
