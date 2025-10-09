d7087519abeed23d41d84d3120ef223c2c92b903
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-09 11:55:04 +0300

    IfAndElseNumbers

---
+++ 05. 01.10.25/RandomColor/RandomColor/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RandomColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iga kord tuleb number suvaline number!");
+
+            int color = new Random().Next(0, 3);
+
+            if (color == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Console.WriteLine("Green");
+            }
+            else if (color == 1)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine("Red");
+            }
+            else if (color == 2)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Clear();
+                Console.WriteLine("Blue");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
