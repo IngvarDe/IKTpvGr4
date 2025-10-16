f1a0ad27ff7ee51383a3347d2a18868b3c0e97c5
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-16 10:16:24 +0300

    SwitchwWithFlowChart

---
+++ 07. 16.10.25/Switch/Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta täht ja vajuta enter");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "a":
+                    Console.WriteLine("Sisestasid tähe a");
+                    break;
+
+                case "e":
+                    Console.WriteLine("Sisestasid tähe e");
+                    break;
+
+                case "i":
+                    Console.WriteLine("Sisestasid tähe i");
+                    break;
+
+                default:
+                    Console.WriteLine("Ei ole vokaal");
+                    break;
+            }
+
+        }
+    }
+}
