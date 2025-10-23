40ff77ecf3fef58445fba0b9032e197b78acd433
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-23 12:47:25 +0300

    SwitchWeekday

---
+++ 08. 23.10.25/SwitchWeekday/SwitchWeekday/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchWeekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //tuleb kasutada switchi
+            //kui on esmaspäev, siis konsoolis kuvatakse
+            //"Esmaspäev on tööpäev"
+            //peate kasutama DayOfWeek enumeratsiooni
+
+            switch (DateTime.Now.DayOfWeek)
+            {   
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Esmaspäev on tööpäev");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Tahaks puhata ja on teisipäev");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Liiga palju on koolis käidud");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Neljapäev ja eelviimane koolipäev");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Lõpuks ometi reede");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Laupäev on puhkus");
+                    break;
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Pühapäev on puhkus");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
