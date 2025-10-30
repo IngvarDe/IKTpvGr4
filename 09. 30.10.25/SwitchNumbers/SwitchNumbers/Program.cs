f8ff3dd5482a3a571e7a3497a8e2cb2e2283ae90
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-30 11:49:11 +0200

    Switch

---
+++ 09. 30.10.25/SwitchNumbers/SwitchNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SwitchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            //peab saama sisestada numbrit ja muudab selle int andmetüübiks
+            int number = Convert.ToInt32(Console.ReadLine());
+            //tuleb teha switch, kus on esimene case 10, teine on 20 ja kolmas on 30
+            //kindlasti tuleb ka default lõppu teha
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Sisestasid 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Sisestasid 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Sisestasid 30");
+                    break;
+                default:
+                    Console.WriteLine("Sisestasid mingi muu numbri");
+                    break;
+            }
+
+        }
+    }
+}
