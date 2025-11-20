4873ff79245780bcd2e830dabe9f02a66f553920
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-11-20 09:19:41 +0200

    SwitchTryCatch

---
+++ 11. 20.11.25/SwitchTryCatch/SwitchTryCatch/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SwitchTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try Catch koos Switch-ga");
+
+            //alguses teha switch kus saab sisestada numbreid 1 ja 2
+            Console.WriteLine("Sisesta number 1 või 2");
+
+            try
+            {
+
+                int input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Sisestasid number 1");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Sisestasid number 2");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta kehtiv number.");
+            }
+            //Miks tuleb see error, kui sisestan
+            //numbri: 444444444444444444444444444444444444444444444444444444444
+            //int on 32 bitine
+            //int .MaxValue = 2 147 483 647
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike.");
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Tekkis ootamatu viga: {ex.Message}");
+            //}
+        }
+    }
+}
