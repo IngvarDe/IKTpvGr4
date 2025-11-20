9ec678760540d554b9fe2c193ff1e271af15ff2d
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-11-20 10:39:45 +0200

    whileDoLoop

---
+++ 11. 20.11.25/whileDo/whileDo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace whileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Do");
+
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                //i++ t'hendab, et sama mis i = i + 1
+                i++;
+            } while (i <= 10);
+            //kui tuli nr 11, siis tingimus ei kehti enam ja tsükkel lõpeb
+        }
+    }
+}
