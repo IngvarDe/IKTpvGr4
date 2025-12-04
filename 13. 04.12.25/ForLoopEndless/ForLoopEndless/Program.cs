b045756a74dd4e384dae3acf80b170fb7ee288e6
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-04 09:36:57 +0200

    ForEndless

---
+++ 13. 04.12.25/ForLoopEndless/ForLoopEndless/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ForLoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop endless");
+
+            //kasutame for loopi
+            ushort i = 0;
+
+            // kui on 10 rida konsoolis, siis lõpetab ära
+            //VIHJE: kasutage break-i
+
+            for (; ; )
+            {
+                Console.WriteLine("i väärtus on: {0}", i);
+                i++;
+                if (i > 10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
