ed479c116710af1dc0687daa5dfbb11f2a180a03
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-04 11:22:41 +0200

    ForLoopnesting

---
+++ 13. 04.12.25/ForKillKoll/ForKillKoll/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //VIHJE: for loop for loopi sees
+
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("see on rida nr {0}", i);
+
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("kill-koll");
+                }
+
+                for (int k = 0; k < 1; k++)
+                {
+                    Console.WriteLine("killadi-koll");
+                }
+            }
+
+
+        }
+    }
+}
