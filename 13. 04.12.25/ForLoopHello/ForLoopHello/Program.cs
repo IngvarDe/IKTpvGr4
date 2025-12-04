48cf9e73c192e62b58c100ba8804577ddd0edc4d
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-04 11:22:58 +0200

    ForLoop3Times

---
+++ 13. 04.12.25/ForLoopHello/ForLoopHello/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //teha konsoolirakendus, kus kasutad for loopi
+            //kolm korda näidatakse Tere tulemast
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("tere tulemast");
+            }
+        }
+    }
+}
