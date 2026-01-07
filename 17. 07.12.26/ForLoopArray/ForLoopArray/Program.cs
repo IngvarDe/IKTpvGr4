db804c7e2ff8ab736671e7dcdf5701a573e72191
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-01-07 14:26:55 +0200

    ForLoopArray

---
+++ 17. 07.12.26/ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "H", "e", "l", "l", "o" };
+
+            //vaja teha string array H, e, l, l, o
+            //kasutada for loopi
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+            //tuleb kasutada foreachi
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
