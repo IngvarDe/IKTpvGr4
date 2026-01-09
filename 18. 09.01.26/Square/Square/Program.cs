4357a3039518a8b10e1c608c05884c30e73faf09
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-01-09 15:09:52 +0200

    ForSquare

---
+++ 18. 09.01.26/Square/Square/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ruudu suurus");
+            //tärnidega (*) teha for loopiga ruut
+
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            //kui suurus on sisestatud, siis kuvab meile ümbermõõdu ja pindala
+            // \n annab ühe tühja rea 
+            Console.WriteLine("\n");
+            int area = size * size;
+            int circumreference = 4 * size;
+            Console.WriteLine("Pindala on " + area);
+            Console.WriteLine("Ümbermõõt on " + circumreference);
+        }
+    }
+}
